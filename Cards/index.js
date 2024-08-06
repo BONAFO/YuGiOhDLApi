03d2874cadea961bc1bea537b6f8c499e0dc15e0
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-08-06 00:12:46 -0300

    Add files via upload

--- Cards/index.js
+++ Cards/index.js
@@ -14,7 +14,8 @@ const path = require("path");
 
 
 
-const MainFolderImgs = "../DB/Images/";
+const MainFolder = "../DB/";
+const MainFolderImgs = MainFolder + "Images/";
 const Cards = MainFolderImgs + "Cards/";
 const Pack = MainFolderImgs + "Packs/";
 
@@ -22,11 +23,16 @@ let br = 0;
 
 
 (() => {
+    try {
+        fs.mkdirSync(MainFolder)
+    } catch (error) {
+        
+    }
 
     try {
         fs.mkdirSync(MainFolderImgs)
     } catch (error) {
-
+        
     }
 
     try {
@@ -161,18 +167,6 @@ const GET_CARD_IMG = async(card, toSave) => {
 
 
 
-    async function downloadImage(url, outputPath) {
-        try {
-            const response = await axios.get(url, { responseType: 'arraybuffer' });
-
-            // Escribe la imagen en el archivo
-            fs.writeFileSync(outputPath, response.data);
-            console.log('Imagen descargada y guardada en', outputPath);
-        } catch (error) {
-            console.error('Error al descargar o guardar la imagen:', error);
-        }
-    }
-
 
     const urls = [];
     for (let i = 0; i < final.length; i++) {
@@ -209,6 +203,19 @@ const GET_CARD_IMG = async(card, toSave) => {
 }
 
 
+async function downloadImage(url, outputPath) {
+    try {
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
+
+        // Escribe la imagen en el archivo
+        fs.writeFileSync(outputPath, response.data);
+        console.log('Imagen descargada y guardada en', outputPath);
+    } catch (error) {
+        console.error('Error al descargar o guardar la imagen:', error);
+    }
+}
+
+
 const getCards = async(pack) => {
     const BASE = "https://www.konami.com/yugioh/duel_links/en/box/";
     let resp = (await axios.get(BASE + pack.href)).data;
@@ -221,25 +228,37 @@ const getCards = async(pack) => {
     let content = dom.querySelectorAll("li");
 
     const cards = [];
-    for (let i = 0; i < content.length; i++) {
+
+    // content.length
+    
+    
+    for (let i =0; i <content.length; i++) {
         const card = content[i];
         const card_data = {};
-        card_data["name"] = card.querySelector("dt").textContent;
+        card_data["name"] = card.querySelector("dt").textContent.replaceAll('"""', '"');
+        card_data["name"] =   (card_data["name"].replaceAll(" The ", " the ")).replaceAll(" Of ", " of ")
         card_data["rarity"] = Array.from(card.querySelector("a").classList).filter(cl => cl.includes("rare"))[0].replace("rare-", "").toUpperCase();
         // card_data["description"] = await get_card_description(card_data["name"])
-        cards.push(card_data)
-
+        const moredata = await get_card_description(card_data["name"] );
+        card_data["fn"] = (card_data["name"].replaceAll('"', "")).replace(" ", "_");
+        cards.push({...card_data, ...moredata})
     }
 
-    console.log(cards[8]);
-
-
-
-
-
-
+    for (let i = 0; i < cards.length; i++) {
+        const ci = cards[i] ;
+        try {
+            downloadImage(ci.img, Cards + ci.fn + ".png")
+        } catch (error) {
+            console.log(error);
+            
+        }
+        
+    }
+    
+    fs.writeFileSync(MainFolderImgs + "manifiest.json", JSON.stringify(cards))
 
 }
+
 
 
 const getIndex = (word, txt, stopIn) => {
@@ -266,134 +285,168 @@ const get_card_description = async(cardName) => {
 
     cardName = cardName.replaceAll(" ", "_");
     const base = `https://yugioh.fandom.com/wiki/${cardName}`
-    console.log(base);
+
 
     // const base = `https://www.google.com/search?q=${cardName}+description&sca_esv=981a0d32bcbf9b3f&hl=es&sxsrf=ADLYWIKz_sD6vJukefLRfl3iz0iEBHGybQ%3A1722871989272&ei=tfCwZseoEIHx1sQPiJOygQM&ved=0ahUKEwiH16enlt6HAxWBuJUCHYiJLDAQ4dUDCBA&uact=5&oq=Infernoble+Arms+-+Joyeuse+description&gs_lp=Egxnd3Mtd2l6LXNlcnAiJUluZmVybm9ibGUgQXJtcyAtIEpveWV1c2UgZGVzY3JpcHRpb25I4gNQAFgAcAB4AJABAJgBlAGgAZQBqgEDMC4xuAEDyAEA-AEC-AEBmAIAoAIAmAMAkgcAoAdZ&sclient=gws-wiz-serp`;
-    const response = (await axios.get(base)).data;
-    // let text = response.slice(response.indexOf('<table class="cardtable">'));
-
-    const dom = stringTOhtml(response).window.document;
-
-    const rows = dom.getElementsByClassName("cardtablerow");
-    const cardData = {};
-    let toSearch = ["Card type", 'Property', 'English', "Attribute",
-        "Types",
-        "Level",
-        "ATK",
-        "Link Arrows",
-        'Pendulum Scale'
-    ];
-    let rows_usefull = [];
-    for (let i = 1; i < rows.length; i++) {
-        const row = rows[i];
-
-
-        for (let j = 0; j < toSearch.length; j++) {
-            if (row.outerHTML.includes(toSearch[j])) {
-                rows_usefull.push([row, toSearch[j]])
-                break;
-            }
-
-        }
-
-    }
-
-
-
-
-
-    for (let i = 0; i < rows_usefull.length; i++) {
-        let row = rows_usefull[i][0].textContent;
-
-        if (row.includes("English") && !row.includes("TCG sets")) {
-
-
-            const lang_break = [
-                "French",
-                "German",
-                "Italian",
-                "Portuguese",
-                "Spanish",
-                "Japanese",
-                "Korean",
-
-            ]
-
-            const indexs = [row.indexOf("English"), (() => {
-                let end_index = -1;
-                for (let k = 0; k < lang_break.length; k++) {
-                    end_index = row.indexOf(lang_break[k]);
-                    if (end_index != -1) {
-                        end_index -= lang_break[k].length
-                        break
-                    }
-
+    try {
+        const response = (await axios.get(base)).data;
+        // let text = response.slice(response.indexOf('<table class="cardtable">'));
+    
+        const dom = stringTOhtml(response).window.document;
+        let cardImage = dom.getElementsByClassName("cardtable-cardimage")[0].querySelector("img").src;
+        cardImage = cardImage.replace(cardImage.substring(cardImage.indexOf("scale-to-width-down") ,cardImage.indexOf("?")  ), "");
+        
+  
+        
+        const rows = dom.getElementsByClassName("cardtablerow");
+        const cardData = {
+            img : cardImage
+        };
+        let toSearch = ["Card type", 'Property', 'English', "Attribute",
+            "Types",
+            "Level",
+            "ATK",
+            "Link Arrows",
+            'Pendulum Scale',
+            "Rank"
+        ];
+        let rows_usefull = [];
+        for (let i = 1; i < rows.length; i++) {
+            const row = rows[i];
+    
+    
+            for (let j = 0; j < toSearch.length; j++) {
+                if (row.outerHTML.includes(toSearch[j])) {
+                    rows_usefull.push([row, toSearch[j]])
+                    break;
                 }
-                return end_index
-            })()];
-
-
-
-            if (!cardData["description"]) {
-                cardData["description"] = row.slice(indexs[0], indexs[1]).replace("English", "").trim();
+    
             }
-
-        } else if (!row.includes("TCG sets")) {
-            switch (rows_usefull[i][1]) {
-                case "Card type":
-                    cardData["type"] = row.replace("Card type", "").trim();
-                    break;
-
-
-                case "Property":
-                    cardData["types"] = row.replace("Property", "").trim();
-                    break;
-                case "Pendulum Scale":
-                    cardData["pendulum_scale"] = parseInt(row.replace("Pendulum Scale", "").trim());
-                    break;
-
-
-                case "Attribute":
-                    cardData["attibute"] = row.replace("Attribute", "").trim();
-                    break;
-
-
-                case "Types":
-                    cardData["types"] = (row.replace("Types", "").trim()).split("/");
-                    break;
-
-                case "Level":
-                    cardData["level"] = parseInt(row.replace("Level", "").trim());
-                    break;
-
-                case "ATK":
-                    cardData["stats"] = ((row.replace("ATK", "").trim()).split("/").map(val => {
-                        val = val.replace(/\D/g, "");
-                        return parseInt(val)
-                    })).filter(val => !isNaN(val));
-                    break;
-
-
-                case "Link Arrows":
-                    cardData["link_arrows"] = row.replace("Link Arrows", "").trim();
-                    break;
-
-            }
-
-
-
-
+    
         }
+    
+    
+    
+    
+    
+        for (let i = 0; i < rows_usefull.length; i++) {
+            let row = rows_usefull[i][0].textContent;
+    
+            if (row.includes("English") && !row.includes("TCG sets")) {
+    
+    
+                const lang_break = [
+                    "French",
+                    "German",
+                    "Italian",
+                    "Portuguese",
+                    "Spanish",
+                    "Japanese",
+                    "Korean",
+    
+                ]
+    
+                const indexs = [row.indexOf("English"), (() => {
+                    let end_index = -1;
+                    for (let k = 0; k < lang_break.length; k++) {
+                        end_index = row.indexOf(lang_break[k]);
+                        if (end_index != -1) {
+                            end_index -= lang_break[k].length
+                            break
+                        }
+    
+                    }
+                    return end_index
+                })()];
+    
+    
+    
+                if (!cardData["description"]) {
+                    cardData["description"] = row.slice(indexs[0], indexs[1]).replace("English", "").trim();
+                }
+    
+            } else if (!row.includes("TCG sets")) {
+                switch (rows_usefull[i][1]) {
+                    case "Card type":
+                        cardData["type"] = row.replace("Card type", "").trim();
+                        break;
+    
+    
+                    case "Property":
+                        cardData["types"] = row.replace("Property", "").trim();
+                        break;
+                    case "Pendulum Scale":
+                        cardData["pendulum_scale"] = parseInt(row.replace("Pendulum Scale", "").trim());
+                        break;
+    
+    
+                    case "Attribute":
+                    if(cardData["attibute"] == undefined){
+                        cardData["attibute"] = row.replace("Attribute", "").trim();
+                    }
+                        break;
+    
+    
+                    case "Types":
+                        cardData["types"] = (row.replace("Types", "").trim()).split("/");
+                        break;
+    
+                        case "Rank":
+                    case "Level":
+                       if(cardData["level"] == undefined && !row.includes("Materials")){
+                 
+                        
+                        cardData["level"] = parseInt(row.replace(/\D/g, ""));
+                       }
+                        break;
+    
+                    case "ATK":
+                    if(cardData["stats"] == undefined){
+                        cardData["stats"] = ((row.replace("ATK", "").trim()).split("/").map(val => {
+                            val = val.replace(/\D/g, "");
+                            return parseInt(val)
+                        })).filter(val => !isNaN(val));
+                    }
+                        break;
+    
+    
+                    case "Link Arrows":
+                        cardData["link_arrows"] = row.replace("Link Arrows", "").trim();
+                        break;
+    
+                }
+    
+    
+    
+    
+            }
+    
+    
+    
+    
+        }
+    
+    
+    
+        return cardData
+    
+    } catch (error) {
 
+console.log(error);
 
+        return {}
+        // console.log(cardName);
+        // if(error.response.status != 404){
+   
+            
+        //     console.log(error);
+            
+        // }
 
-
+        
+        
+           
     }
-
-
-
-    return cardData
-
 
 
 
@@ -430,4 +483,4 @@ app.listen(3000, () => {
 
 
 
-// getCards(manifiest["main_box"][0])
+getCards(manifiest["main_box"][0])
